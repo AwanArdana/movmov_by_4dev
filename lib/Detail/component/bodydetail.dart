81cdbc88db357370a1275903be426f62c3c7dd1c
ari_akm <aremaakm@gmail.com>
2022-09-27 14:00:13 +0700

    Awan - Ads Banner

--- lib/Detail/component/bodydetail.dart
+++ lib/Detail/component/bodydetail.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:movmov/Detail/component/titlewithdetail.dart';
 import 'package:movmov/Player/player_screen.dart';
 import 'package:movmov/Detail/component/headerdetail.dart';
@@ -8,6 +9,7 @@ import 'package:movmov/constants.dart';
 import 'package:movmov/fungsi_kirim_web_service.dart';
 
 
+import '../../Ads/ad_helper.dart';
 import 'DetailReadMore.dart';
 import 'cover_with_title.dart';
 
@@ -17,22 +19,6 @@ class BodyDetail extends StatelessWidget{
   final List list;
   final String Mov_id;
 
-  // Future<List> getGenres() async{
-  //   final response = await http.get(Uri.parse("https://awanapp.000webhostapp.com/getmovgenre.php?id=" + Mov_id));
-  //   return json.decode(response.body);
-  // }
-
-
-
-  // InAppWebViewGroupOptions _options = InAppWebViewGroupOptions(
-  //   android: AndroidInAppWebViewOptions(
-  //     useHybridComposition: true,
-  //   )
-  // );
-  // InAppWebViewController webView;
-  // String Url = "https://drive.google.com/file/d/18bcKYHpxizSFvdwT4fBlSG_PNridmpnc/preview";
-  // bool status = true;
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -41,31 +27,18 @@ class BodyDetail extends StatelessWidget{
       child: Column(
         children: <Widget>[
           HeaderDetail(size: size),
-          /*new FutureBuilder<List>(
-            future: getData(),
-            builder: (context, snapshot){
-              if(snapshot.hasError) print(snapshot.error);
-
-              return snapshot.hasData
-                  ? new Detail(list: snapshot.data,size: size,)
-                  : new Center(child: new CircularProgressIndicator(),);
-            },
-
-          ),*/
           new FutureBuilder<List>(
-            // future: getGenres(Mov_id),
-            // future: getDataGlobal("getmovgenre.php?id=", Mov_id),
+            //list genres
             future: SQLEksek("SELECT gen_title FROM genres g, movie_genres mg WHERE g.gen_id=mg.gen_id and mg.mov_id=" + Mov_id),
             builder: (context, snapshot){
               if(snapshot.hasError) print(snapshot.error);
 
               return snapshot.hasData
                   ? new Detail(list: list, size: size, Mov_id: Mov_id, listGenre: snapshot.data,)
-                  // :  new Center(child: new CircularProgressIndicator(),);
-                  : Detail(list: list, size: size, Mov_id: Mov_id,listGenre: [],);
+                  :  new Center(child: new CircularProgressIndicator(),);
+                  // : Detail(list: list, size: size, Mov_id: Mov_id,listGenre: [],);
             },
           ),
-          // Detail(list: list,size: size, Mov_id: Mov_id,)
 
         ],
       ),
@@ -74,7 +47,7 @@ class BodyDetail extends StatelessWidget{
 
 }
 
-class Detail extends StatelessWidget{
+class Detail extends StatefulWidget{
 
   const Detail({
     Key key, this.list, this.size, this.Mov_id, this.listGenre,
@@ -90,86 +63,140 @@ class Detail extends StatelessWidget{
   //   return json.decode(response.body);
   // }
 
+  Future<InitializationStatus> _initGoogleMobileAds() {
+    // TODO: Initialize Google Mobile Ads SDK
+    return MobileAds.instance.initialize();
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //
+  // }
+
+  @override
+  _Detail createState() => _Detail();
+
+}
+
+class _Detail extends State<Detail>{
+  // TODO: Add _bannerAd
+  BannerAd _bannerAd;
+
+  @override
+  void initState(){
+    BannerAd(
+        adUnitId: AdHelper.bannerAdUnitId,
+        request: AdRequest(),
+        size: AdSize.banner,
+        listener: BannerAdListener(
+            onAdLoaded: (ad){
+              setState(() {
+                _bannerAd = ad as BannerAd;
+              });
+            },
+            onAdFailedToLoad: (ad, err){
+              print("Failed to load a banner ad: ${err.message}");
+              ad.dispose();
+            }
+        )
+    ).load();
+  }
+
+  @override
+  void dispose(){
+    // TODO: Dispose a BannerAd object
+    _bannerAd.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new FutureBuilder(
       //list episode
-      future: SQLEksek("SELECT e.episode_id, e.episode from episode e WHERE e.mov_id=" + Mov_id),
+      future: SQLEksek("SELECT e.episode_id, e.episode from episode e WHERE e.mov_id=" + widget.Mov_id),
       builder: (context, snapshot){
         if (snapshot.hasError) print(snapshot.error);
 
         return snapshot.hasData
             ? new Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CoverWithTitle(
-                      size: size,
-                      title: "${list[0]['mov_title']}",
-                      coverId: "${list[0]['mov_cover_id']}",
-                      listGenre: listGenre,
-                      mov_id: Mov_id,
-                      listEpisode: snapshot.data,
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CoverWithTitle(
+                size: widget.size,
+                title: "${widget.list[0]['mov_title']}",
+                coverId: "${widget.list[0]['mov_cover_id']}",
+                listGenre: widget.listGenre,
+                mov_id: widget.Mov_id,
+                listEpisode: snapshot.data,
+              ),
 
-                    DetailWithReadMore(Detail: "${list[0]['mov_deskripsi']}",),
-
-                    new ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      padding: const EdgeInsets.all(8),
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (BuildContext context, int index){
-                        return Container(
-                          padding: const EdgeInsets.all(8),
-                          height: 50,
-                          color: kPrimaryColor,
-                          child: Row(
-                            children: [
-                              Text(
-                                  'Episode ${snapshot.data[index]['episode']}'
-                              ),
-                              Spacer(),
-
-                              TextButton(
-                                style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.resolveWith(
-                                            (Set<MaterialState> states){
-                                          if(states.contains(MaterialState.pressed)) return kSecondaryColor.withOpacity(0.5);
-                                          return kSecondaryColor;
-                                        }
-                                    ),
-                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(20),
-                                        )
-                                    )
-                                ),
-                                // shape: RoundedRectangleBorder(
-                                //   borderRadius: BorderRadius.circular(20)
-                                // ),
-                                // color: kSecondaryColor,
-                                onPressed: (){
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => new PlayerScreen(Episode: "${snapshot.data[index]['episode']}", listEpisode: snapshot.data, listGenre: listGenre, mov_id: Mov_id,),
-                                      )
-                                  );
-                                },
-                                child: Text(
-                                  "Play",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                    )
-                  ],
+              DetailWithReadMore(Detail: "${widget.list[0]['mov_deskripsi']}",),
+              //ads disini
+              if(_bannerAd != null)
+                Container(
+                  width: _bannerAd.size.width.toDouble(),
+                  height: _bannerAd.size.width.toDouble() * 0.3,
+                  child: AdWidget(ad: _bannerAd,),
                 ),
+
+              new ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(8),
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Container(
+                    padding: const EdgeInsets.all(8),
+                    height: 50,
+                    color: kPrimaryColor,
+                    child: Row(
+                      children: [
+                        Text(
+                            'Episode ${snapshot.data[index]['episode']}'
+                        ),
+                        Spacer(),
+
+                        TextButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.resolveWith(
+                                      (Set<MaterialState> states){
+                                    if(states.contains(MaterialState.pressed)) return kSecondaryColor.withOpacity(0.5);
+                                    return kSecondaryColor;
+                                  }
+                              ),
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  )
+                              )
+                          ),
+                          // shape: RoundedRectangleBorder(
+                          //   borderRadius: BorderRadius.circular(20)
+                          // ),
+                          // color: kSecondaryColor,
+                          onPressed: (){
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => new PlayerScreen(Episode: "${snapshot.data[index]['episode']}", listEpisode: snapshot.data, listGenre: widget.listGenre, mov_id: widget.Mov_id,),
+                                )
+                            );
+                          },
+                          child: Text(
+                            "Play",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
               )
+            ],
+          ),
+        )
             : new Center(child: new CircularProgressIndicator(),);
       },
     );
